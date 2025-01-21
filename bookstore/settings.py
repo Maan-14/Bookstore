d5e54ec9ebe638407e1ddba7d97e298d37bc8324
Maan <marmughanjaved@gmail.com>
2025-01-21 19:26:30 +0500

    handled the user media files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,6 +148,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+
+MEDIA_URL = "/media/" # new
+MEDIA_ROOT = BASE_DIR / "media" # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
